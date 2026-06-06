2cbb77c27d594f48194dfc278704215e3f9c9daf
Niyoosha <niyoosha.tahami@masterly.ca>
2026-06-05 23:49:11 -0400

    bug fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
